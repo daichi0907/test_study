c5e058de7ef7237fc1a14bc8a304756f38a7c502
daichi0907 <daichi.watanabe0907@icloud.com>
2022-05-15 00:50:52 +0900

    4つのテストコードと実装を行いました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -8,5 +8,15 @@
 
 bool is_leap_year(int year)
 {
-	return true;
+	// 100で割り切れた際は平年の可能性がある
+	if (year % 100)
+	{
+		// 400で割り切れたらうるう年
+		return (year % 400) == 0;
+	}
+	// 上記以外で4で割り切れる年はうるう年
+	else
+	{
+		return (year % 4) == 0;
+	}
 }
